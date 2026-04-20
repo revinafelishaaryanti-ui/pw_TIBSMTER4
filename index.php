b14478a54f306dc1bd400a0c96ec8753011fcc4a
revinafelishaaryanti-ui <revinafelishaaryanti@gmail.com>
2026-04-20 08:47:51 +0800

    update materi 2

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
 echo '<br><br><a href="materi1.php"><button>ke materi 1 : if dan else</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 2 :  looping</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 3 : if dan else</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 4 : if dan else</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 5 : if dan else</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 6 : if dan else</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 7 : if dan else</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 8 : if dan else</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 9 : if dan else</button></a>' ;
-echo '<br><br><a href="materi1.php"><button>ke materi 10 : if dan else</button></a>' ;
+echo '<br><br><a href="materi2.php"><button>ke materi 2 :  looping</button></a>' ;
+echo '<br><br><a href="materi3.php"><button>ke materi 3 : if dan else</button></a>' ;
+echo '<br><br><a href="materi4.php"><button>ke materi 4 : if dan else</button></a>' ;
+echo '<br><br><a href="materi5.php"><button>ke materi 5 : if dan else</button></a>' ;
+echo '<br><br><a href="materi6.php"><button>ke materi 6 : if dan else</button></a>' ;
+echo '<br><br><a href="materi7.php"><button>ke materi 7 : if dan else</button></a>' ;
+echo '<br><br><a href="materi8.php"><button>ke materi 8 : if dan else</button></a>' ;
+echo '<br><br><a href="materi9.php"><button>ke materi 9 : if dan else</button></a>' ;
+echo '<br><br><a href="materi10.php"><button>ke materi 10 : if dan else</button></a>' ;
 
 ?>
---
+++ materi2.php
@@ -0,0 +1,17 @@
+<form method="post">
+    Masukan sebuah Angka : <input type="number" name="angka">
+    <input type="submit" value="kirim">
+</form>
+
+<?php
+if (isset($_POST['angka'])) {
+    $angka = $_POST['angka'];
+
+    for ($i = 1; $i <= $angka; $i++) {
+        echo "<br>Nilai Anda : $i";
+    }
+}
+
+
+
+?>
